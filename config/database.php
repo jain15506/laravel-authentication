963d3ab631b377557a075ac4bb7703513a4b3740
jain15506 <jain15506@gmail.com>
2016-03-31 08:48:51 +0530

    updated

--- config/database.php
+++ config/database.php
@@ -55,9 +55,9 @@ return [
 		'mysql' => [
 			'driver'    => 'mysql',
 			'host'      => env('DB_HOST', 'localhost '),
-			'database'  => env('DB_DATABASE', 'opagallery'),
-			'username'  => env('DB_USERNAME', 'parul'),
-			'password'  => env('DB_PASSWORD', '1YYYnnnYYY'),
+			'database'  => env('DB_DATABASE', 'root'),
+			'username'  => env('DB_USERNAME', 'root'),
+			'password'  => env('DB_PASSWORD', ''),
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
